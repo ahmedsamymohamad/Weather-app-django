c4dba793d24c8bbbbfc2f8f950236dc40277aa1f
ahmedsamymohamed <ahmed01223330@gmail.com>
2024-02-19 20:43:23 +0200

    First commit

--- weather/wsgi.py
+++ weather/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
 
 application = get_wsgi_application()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+static_root = os.path.join(BASE_DIR, 'staticfiles')
+
+# Wrap the Django application with WhiteNoise
+application = WhiteNoise(application, root=static_root)
