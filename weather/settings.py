aa4ca09f3c91f064af999537df6af6b9cdf47058
ahmedsamymohamed <ahmed01223330@gmail.com>
2024-02-19 20:32:18 +0200

    First commit

--- weather/settings.py
+++ weather/settings.py
@@ -25,9 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.environ['DEBUG'] ==  "True"
 
 
 # Application definition
